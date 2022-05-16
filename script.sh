d0df739e37424f43ac32228eb855e38a50dd984f
dmitro <dmitro@localhost.localdomain>
2022-05-16 21:39:38 +0300

    ADD: pdf convertion, cli flags, some flexibility

--- script.sh
+++ script.sh
@@ -1,33 +1,93 @@
 #!/bin/bash
 
-url=$1
-dir="$(pwd)/out"
-dir_h="${dir}/src"
-counter=1
-cur_dir="${dir_h}$counter"
+usage(){
+    echo "usage: ${0##*/} script [-H | --help]"
+    echo -e "\t-U, --url        url of the book source"
+    echo -e "\t-D, --directory  dir to save images"
+    echo -e "\t-P, --pdf        to convert images into one pdf (by default 'out.pdf')"
+    echo -e "\t-H, --help       print to the output this message"
+}
 
-if [ ! -e $dir ]
+error(){
+    echo -e "error occured: $1"
+    usage
+}
+
+rewrite_out(){
+    echo -e "Directory 'out' already exists !"
+    echo -e "You haven't specified directory for NEW source !"
+    echo -e "delete 'out' or move to another place"
+    exit 2
+}
+
+### some variables
+url=""
+url_flag=0
+dir="$(pwd)/out"
+pdf_conv_flag=0
+pdf_name="out.pdf"
+###
+
+#flags input
+while [ ! -z "$1" ];
+do
+    case "$1" in
+        --url | -U )        
+            shift
+            url=$1
+            url_flag=1
+            ;;
+        --directory | -D )   
+            shift
+            dir="$(pwd)/$1"
+            ;;
+        --pdf | -P )
+            shift
+            pdf_conv_flag=1
+            if [ ! -z "$1" ]
+            then
+                pdf_name=$1
+            fi
+            ;;
+        --help | -H )       
+            usage
+            exit 0
+            ;;
+        * )                 
+            error "unknown argument"
+            exit 1
+            ;;
+    esac
+shift
+done
+
+#check for newbies
+if [ $url_flag == 0 ];
+then
+    usage
+    exit 4
+fi
+
+#check for dummies
+if [ ! -e $dir ];
 then		
     mkdir $dir 
-	mkdir ${dir_h}$counter
-	wget -P "${dir_h}$counter" -O "${dir_h}$counter/source" $url
+	wget -P "${dir}" -O "${dir}/source" $url
 else
-	file_num="$(ls $dir -1bA | wc -l)"
-	cur_dir="${dir_h}$((file_num+1))"
-	mkdir $cur_dir 
-	wget -P $cur_dir -O "$cur_dir/source" $url
+    rewrite_out
 fi
 
 ###########
-total_size="$(grep '<span id="bmkpagetotalnum">' "$cur_dir/source")"
+total_size="$(grep '<span id="bmkpagetotalnum">' "$dir/source")"
 total_size=${total_size/\<\/span\>\<\/div\>}
 total_size=${total_size/\<div class\=\"CurPage\"\>\С\т\р\. \<span id\=\"bmkpagenum\" class\=\"CurPageScroller\"\>\1\<\/span\> \/\<span id\=\"bmkpagetotalnum\"\>}
 extra_sub=${total_size:0: 12}
 total_size=${total_size/$extra_sub}
 total_size=${total_size:: -1} #4
+###########
 
 ###########
-global_key="$(grep '000001' "$cur_dir/source")"
+global_key="$(grep '000001' "$dir/source")"
 extra_val=${global_key:0: 440}
 global_key=${global_key/$extra_val}
 ###########
@@ -45,17 +105,24 @@ global_key=${global_key/\000\001\.png\" style\=\"float\:left\;text\-align\:cente
 global_key=${global_key::-1}
 ###########
 
+#%%%%%%%
 extra="00000"
 for ((i=1; i<$total_size; i++))
 do
 	str="$extra$i"
-	if [[ ${#str} -ne 6 ]] #if not euq
+	if [ ${#str} -ne 6 ]; #if not euq
 	then
 		extra=${extra:1}
 		str="$extra$i"		
 	fi
 	out="http://elibrary.mai.ru/ProtectedView/Content/tmp/${global_key}${str}.png"
-	wget -q -P "$cur_dir" $out
+	wget -q -P "$dir" $out
 done
+#%%%%%%%
 
-#find "$PWD" -iname '*.png' -execdir convert '{}' '{}'.pdf \;
+#pdf convertion (optional)
+#requires 'ghostscript' and 'ImageMagick' dependensies
+if [ $pdf_conv_flag == 1 ];
+then
+    convert "$dir/*.png" $pdf_name
+fi
